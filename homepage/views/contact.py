7131466df24851d386d9f1bd2f1dc5e8c7ad45cf
rem-rogers <remington.rogers.94@gmail.com>
2018-01-24 10:32:31 -0700

    s0 uploaded

---
+++ homepage/views/contact.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django_mako_plus import view_function, jscontext
+from datetime import datetime, timezone
+
+@view_function
+def process_request(request):
+
+
+    if request.method == 'POST':
+        print(request.POST['fname'])
+        print(request.POST['lname'])
+
+
+
+    context = {
+
+    }
+    return request.dmp_render('contact.html', context)
