492e8fd208d4b0102fc0064364affbf76c565eb8
rem-rogers <remington.rogers.94@gmail.com>
2018-03-08 22:10:38 -0700

    done except for making categories active

--- catalog/scripts/index.js
+++ catalog/scripts/index.js
@@ -6,16 +6,20 @@ $(function(context){
         console.log(context.pNum);
 
         $('#next_page').click(function(){
-            context.pNum++;
-            $("#currentPage").text(context.pNum);
-            $("#catalog").load("/catalog/index.products/" + context.category + "/" + context.pNum)
-        });
+            if (context.pNum < context.pMax) {
+                context.pNum++;
+                $("#currentPage").text(context.pNum);
+                $("#catalog").load("/catalog/index.products/" + context.category + "/" + context.pNum)
+            }
+            });
 
         $('#previous_page').click(function() {
+            if (context.pNum >1) {
             context.pNum--;
             $("#currentPage").text(context.pNum);
             $("#catalog").load("/catalog/index.products/" + context.category + "/" + context.pNum)
-        });
+             }
+            });
 
 
 
