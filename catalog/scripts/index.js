2a9c80c3ba3c1df5373c0ed61b9155eb461b69ed
rem-rogers <remington.rogers.94@gmail.com>
2018-03-08 18:06:47 -0700

    got total page count

--- catalog/scripts/index.js
+++ catalog/scripts/index.js
@@ -4,8 +4,9 @@ $(function(context){
         $("#catalog").load("/catalog/index.products/" + context.category + "/" + context.pnum + "/")
         console.log(context.category);
         console.log(context.pnum);
-        // $('#next_page').load()
-        //
-        // $('#previous_page').load()
+
+        $('#next_page').load()
+
+        $('#previous_page').load()
     }
 }(DMP_CONTEXT.get()));
--- catalog/views/index.py
+++ catalog/views/index.py
@@ -1,16 +1,29 @@
 from django_mako_plus import view_function, jscontext
 from datetime import datetime
 from catalog import models as cmod
+import math
 
 
 @view_function
-def process_request(request, cat_id=0):
+def process_request(request, cat_id=0, pNum=1):
     category = cat_id
     cat = cmod.Category.objects.all()
+    if category != 0:
+        page = cmod.Product.objects.filter(category_id=cat_id).count()
+    else:
+        page = cmod.Product.objects.all().count()
+    page = page/6
+    page = math.ceil(page)
+
+    currentPage = page
+
     context = {
         'cat': cat,
         jscontext('category'): category,
-        jscontext('pnum'): 1,
+        jscontext('pNum'): pNum,
+        jscontext('pMax'): page,
+        'page': page,
+        'currentPage': currentPage,
     }
     return request.dmp.render('index.html', context)
 
@@ -19,12 +32,16 @@ def products(request, cat: cmod.Category=None, pnum: int=1):
     # qry = cmod.Product.objects.all()
     if cat is not None:
         qry = cmod.Product.objects.filter(category_id=cat.id)
+        cName = cat
     else:
         qry = cmod.Product.objects.all()
+        cName = 'All Products'
 
     qry = qry[((pnum - 1)*6):6*pnum] #pagination
+
     context = {
         'qry': qry,
         'pnum': pnum,
+        'cName': cName,
     }
     return request.dmp.render('index.products.html', context) #inherits from base_ajax
